22eb9a0d22a276a4b32a3f6f24a0539193fe210d
schwarz-weiss-reinigung <karl.mirrorsinnovation@gmail.com>
2025-11-20 17:03:15 +0500

    Update index.php

--- index.php
+++ index.php
@@ -73,6 +73,7 @@ $schemaLocalBusiness = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-site-verification" content="3mzUiSIoLmrq_m8ZCPRRbJlnHo78RZncHEqYA3HMC7g" />
     <meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">
     <meta name="description" content="<?php echo htmlspecialchars($metaDescription); ?>">
     <meta name="keywords" content="<?php echo htmlspecialchars($keywords); ?>">
